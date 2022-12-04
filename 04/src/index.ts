dfff6f22ba8289f689ddf9d8b155d38396590b8f
Max Tapp <max.tapp@resultstack.com>
2022-12-04 16:23:14 -0600

    part one done

--- 04/src/index.ts
+++ 04/src/index.ts
@@ -1,8 +1,41 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
-export const testFunction = (): string => {
-  console.log('testing')
-  return 'testing';
+interface PartnerAssignment {
+  readonly rangeOne: string;
+  readonly rangeTwo: string;
+  readonly rangeOneInsideRangeTwo: boolean;
+  readonly rangeTwoInsideRangeOne: boolean;
+  readonly assignmentsFullyContained: boolean;
+}
+
+export const determineIfAssignmentsAreInclusive = (partnerAssignments: string): PartnerAssignment => {
+  const [rangeOne, rangeTwo ] = partnerAssignments.split(',');
+  const [lowNumberOne, highNumberOne] = rangeOne.split('-').map(Number);
+  const [lowNumberTwo, highNumberTwo] = rangeTwo.split('-').map(Number);
+  const rangeOneInsideRangeTwo = lowNumberOne >= lowNumberTwo && highNumberOne <= highNumberTwo;
+  const rangeTwoInsideRangeOne = lowNumberTwo >= lowNumberOne && highNumberTwo <= highNumberOne;
+  return {
+    rangeOne,
+    rangeTwo,
+    rangeTwoInsideRangeOne,
+    rangeOneInsideRangeTwo,
+    assignmentsFullyContained: rangeOneInsideRangeTwo || rangeTwoInsideRangeOne
+  }
+}
+
+export const main = (): number => {
+  // read in the file
+  const rawFileData = readFileSync(resolve(__dirname, 'section-assignments.txt'));
+  // parse it
+  const sectionAssignmentsData = rawFileData.toString().split('\n').filter(x => !!x);
+  // get data about each row
+  const partnerAssigments = sectionAssignmentsData.map(determineIfAssignmentsAreInclusive);
+  // how many assignment pairs does one range fully contain the other
+  const containedAssignmentsCount = partnerAssigments.filter(({assignmentsFullyContained}) => assignmentsFullyContained).length;
+  console.log({containedAssignmentsCount});
+  return containedAssignmentsCount;
 }
 
 // for node runs
-testFunction();
+main();
--- 04/src/tests/index.spec.ts
+++ 04/src/tests/index.spec.ts
@@ -1,9 +1,46 @@
 import { expect } from 'chai';
-import { testFunction } from '..';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { determineIfAssignmentsAreInclusive } from '../index';
 
 describe(`Day 4`, () => {
-  it(`should return 'testing'`, () => {
-    const test = testFunction();
-    expect(test).to.equal('testing');
+  describe(`data`, () => {
+    it(`should provide the data in order, right?`, () => {
+      // read in the file
+      const rawFileData = readFileSync(resolve(__dirname, 'section-assignments.txt'));
+      // parse it
+      const sectionAssignmentsData = rawFileData.toString().split('\n').filter(x => !!x);
+      sectionAssignmentsData.forEach(dataRow => {
+        const [rangeOne, rangeTwo ] = dataRow.split(',');
+        const [lowNumberOne, highNumberOne] = rangeOne.split('-').map(Number);
+        const [lowNumberTwo, highNumberTwo] = rangeTwo.split('-').map(Number);
+        expect(lowNumberOne).to.be.lessThanOrEqual(highNumberOne);
+        expect(lowNumberTwo).to.be.lessThanOrEqual(highNumberTwo);
+      });
+    });
+  });
+
+  describe(`determineIfAssignmentsAreInclusive`, () => {
+    it(`should correctly calculate inclusive ranges`, () => {
+      interface AssignmentTest {
+        readonly ranges: string;
+        readonly expectedResult: 'inclusive' | 'exclusive-or-overlap'
+      }
+
+      const testAssignments: AssignmentTest[] = [
+        {ranges: '2-4,1-5', expectedResult: 'inclusive'},
+        {ranges: '2-4,3-5', expectedResult: 'exclusive-or-overlap'},
+        {ranges: '1-5,2-4', expectedResult: 'inclusive'},
+        {ranges: '200-400,1-5', expectedResult: 'exclusive-or-overlap'},
+        {ranges: '2-400,80-500', expectedResult: 'exclusive-or-overlap'},
+        {ranges: '1-400,100-105', expectedResult: 'inclusive'}
+      ];
+
+      testAssignments.forEach(({ranges, expectedResult}) => {
+        const {assignmentsFullyContained: actualResult} = determineIfAssignmentsAreInclusive(ranges)
+        const compValue = expectedResult === 'inclusive';
+        expect(compValue).to.equal(actualResult);
+      });
+    });
   });
 });
