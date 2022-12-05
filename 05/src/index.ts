7587ebfa2ae793d24f17de209f8f7815fde0d2e1
Max Tapp <max.tapp@resultstack.com>
2022-12-05 00:19:35 -0600

    calling it a night

--- 05/src/index.ts
+++ 05/src/index.ts
@@ -1,8 +1,56 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
-export const main = (): number => {
-  console.log('Hello!');
-  return 0;
+export const isNullOrUndefinedOrWhitespace = (value: unknown): boolean => {
+  if (value === null) return true;
+  if (value === undefined) return true;
+  if (typeof value === 'string' && value.trim() === '') return true;
+  return false;
 }
+
+export const getContainerAtPosition = (row: string, position: number): string => {
+  // each column is 4 characters, open bracket, the contents, close bracket, and a space.
+  const startIndex = (position - 1) * 4;
+  return row.charAt(startIndex + 1);
+}
+
+export const initializeContainerMap = (containerData: string[]): Array<string[]> => {
+  const container = new Array<string[]>(9).fill([]);
+  // for each row starting at the bottom, go through each column and add the letters
+  // the last row is just their identifiers, we don't care about it.
+  for (let i = containerData.length - 2; i >= 0; i--) {
+    const row = containerData[i];
+    console.log({row});
+    // uh oh.. a nested loop... I guess this is ok
+    for (let col = 1; col <= 9; col++) {
+      const containerContent = getContainerAtPosition(row, col);
+      if (isNullOrUndefinedOrWhitespace(containerContent)) {
+        console.log('not pushing', containerContent, 'to position', col);
+        continue;
+      }
+      console.log('pushing', containerContent, 'to position', col);
+      container[col -1].push(containerContent);
+    }
+  }
+  // console.log({container});
+  return container;
+}
+
+export const main = (): string => {
+  // read in the file
+  const rawFileData = readFileSync(resolve(__dirname, './rearrange-procedure.txt'));
+  // preliminary parse - we separate the newline that separates the instructions from the container setup
+  const [containerData, underscoreSeparatedRearrangeInstructionData] = rawFileData.toString().split('\n').join('_').split('__');
+  const rearrangeInstructions = underscoreSeparatedRearrangeInstructionData.split('_');
+  // we've looked at the file, so its kinda like cheating.. but whatever
+  const containerMap = initializeContainerMap(containerData.split('_'));
+  
+  // After the rearrangement procedure completes, what crate ends up on top of each stack?
+  const topOfStack: string[] = [];
+  console.log(topOfStack);
+  return topOfStack.join('');
+}
+
 
 // for node runs
 main();
--- 05/src/tests/index.spec.ts
+++ 05/src/tests/index.spec.ts
@@ -1,8 +1,61 @@
 import { expect } from 'chai';
-import { main } from '../index';
+import { getContainerAtPosition, initializeContainerMap, main } from '../index';
 
-describe(`Test`, () => {
-  it(`shouldn't blow up`, () => {
-    expect(main()).to.equal(0);
+describe(`index`, () => {
+  describe(`getContainerAtPosition`, () => {
+    it(`should return the proper container`, () => {
+      const input = '[F] [G] [H] [Z] [N] [P] [M] [N] [D]';
+      const expectedResults = [
+        [1, 'F'],
+        [2, 'G'],
+        [3, 'H'],
+        [4, 'Z'],
+        [5, 'N'],
+        [6, 'P'],
+        [7, 'M'],
+        [8, 'N'],
+        [9, 'D'],
+      ];
+      expectedResults.forEach(([position, expectedResult]) => {
+        expect(getContainerAtPosition(input, +position)).to.equal(expectedResult);
+      });
+    });
+  });
+  describe(`initializeContainerMap`, () => {
+    it(`should return the proper container`, () => {
+      const input = [
+        '    [H]         [D]     [P]        ',
+        '[W] [B]         [C] [Z] [D]        ',
+        '[T] [J]     [T] [J] [D] [J]        ',
+        '[H] [Z]     [H] [H] [W] [S]     [M]',
+        '[P] [F] [R] [P] [Z] [F] [W]     [F]',
+        '[J] [V] [T] [N] [F] [G] [Z] [S] [S]',
+        '[C] [R] [P] [S] [V] [M] [V] [D] [Z]',
+        '[F] [G] [H] [Z] [N] [P] [M] [N] [D]',
+        '1   2   3   4   5   6   7   8   9 '
+      ];
+      const expectedResults = JSON.stringify([
+        ['F', 'C', 'J', 'P', 'H', 'T', 'W'],
+        ['G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H'],
+        ['H', 'P', 'T', 'R'],
+        ['Z', 'S', 'N', 'P', 'H', 'T'],
+        ['N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D'],
+        ['P', 'M', 'G', 'F', 'W', 'D', 'Z'],
+        ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'],
+        ['N', 'D', 'S'],
+        ['D', 'Z', 'S', 'F', 'M']
+      ]);
+      const containerData = JSON.stringify(initializeContainerMap(input));
+      console.log({expectedResults, containerData});
+      expect(containerData).to.equal(expectedResults);
+      // expectedResults.forEach((contents, i) => {
+      //   for (let col = 0; col < 9; col++) {
+      //     const expected = containerData[i][col];
+      //     const actual = contents[i][col];
+      //     expect(actual).to.equal(expected);
+      //     console.log({expected, actual });
+      //   }
+      // });
+    });
   });
 });
