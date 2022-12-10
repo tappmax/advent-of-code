60cecb1b395e725cfde4b46b2289b37f3b31966f
Max Tapp <max.tapp@resultstack.com>
2022-12-10 17:21:58 -0600

    day 5 donezo

--- 05/src/index.ts
+++ 05/src/index.ts
@@ -1,56 +1,125 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import {readFileSync} from 'fs';
+import {resolve} from 'path';
 
 export const isNullOrUndefinedOrWhitespace = (value: unknown): boolean => {
   if (value === null) return true;
   if (value === undefined) return true;
   if (typeof value === 'string' && value.trim() === '') return true;
   return false;
-}
+};
 
-export const getContainerAtPosition = (row: string, position: number): string => {
+export const getContainerAtPosition = (
+  row: string,
+  position: number
+): string => {
   // each column is 4 characters, open bracket, the contents, close bracket, and a space.
   const startIndex = (position - 1) * 4;
   return row.charAt(startIndex + 1);
-}
+};
 
-export const initializeContainerMap = (containerData: string[]): Array<string[]> => {
-  const container = new Array<string[]>(9).fill([]);
+export const initializeContainerMap = (
+  containerData: string[]
+): Map<number, string[]> => {
+  const container = new Map<number, string[]>([
+    [1, []],
+    [2, []],
+    [3, []],
+    [4, []],
+    [5, []],
+    [6, []],
+    [7, []],
+    [8, []],
+    [9, []],
+  ]);
   // for each row starting at the bottom, go through each column and add the letters
   // the last row is just their identifiers, we don't care about it.
   for (let i = containerData.length - 2; i >= 0; i--) {
     const row = containerData[i];
-    console.log({row});
     // uh oh.. a nested loop... I guess this is ok
     for (let col = 1; col <= 9; col++) {
       const containerContent = getContainerAtPosition(row, col);
       if (isNullOrUndefinedOrWhitespace(containerContent)) {
-        console.log('not pushing', containerContent, 'to position', col);
         continue;
       }
-      console.log('pushing', containerContent, 'to position', col);
-      container[col -1].push(containerContent);
+      const colFromContainer = container.get(col) as string[];
+      colFromContainer.push(containerContent);
     }
   }
-  // console.log({container});
   return container;
-}
+};
+
+export const moveOneByOne = (
+  moveAmount: number,
+  fromCol: number,
+  toCol: number,
+  containerMap: Map<number, string[]>
+): void => {
+  const fromStack = containerMap.get(fromCol) as string[];
+  const toStack = containerMap.get(toCol) as string[];
+  for (let i = 0; i < moveAmount; i++) {
+    const poppedContent = fromStack.pop();
+    if (typeof poppedContent !== 'string') {
+      throw new Error(
+        `columns can only container strings, received ${typeof poppedContent}`
+      );
+    }
+    toStack.push(poppedContent);
+  }
+};
+
+export const moveStacks = (
+  moveAmount: number,
+  fromCol: number,
+  toCol: number,
+  containerMap: Map<number, string[]>
+): void => {
+  const fromStack = containerMap.get(fromCol) as string[];
+  const toStack = containerMap.get(toCol) as string[];
+  const fromStackEndIndex = fromStack.length - 1;
+  const newFromStackEndIndex = fromStackEndIndex - moveAmount;
+  // splice mutates the OG array
+  const moveStack = fromStack.splice(newFromStackEndIndex + 1, moveAmount);
+  const newToStack = [...toStack, ...moveStack];
+  containerMap.set(toCol, newToStack);
+};
+
+export const rearrangeStacks = (
+  instructions: string[],
+  containerMap: Map<number, string[]>
+): Map<number, string[]> => {
+  instructions.forEach(instruction => {
+    // we're skipping the english words and grabbing the numbers
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, moveAmount, __, fromCol, ___, toCol] = instruction.split(' ');
+    moveStacks(+moveAmount, +fromCol, +toCol, containerMap);
+  });
+  return containerMap;
+};
 
 export const main = (): string => {
   // read in the file
-  const rawFileData = readFileSync(resolve(__dirname, './rearrange-procedure.txt'));
+  const rawFileData = readFileSync(
+    resolve(__dirname, './rearrange-procedure.txt')
+  );
   // preliminary parse - we separate the newline that separates the instructions from the container setup
-  const [containerData, underscoreSeparatedRearrangeInstructionData] = rawFileData.toString().split('\n').join('_').split('__');
-  const rearrangeInstructions = underscoreSeparatedRearrangeInstructionData.split('_');
+  const [containerData, underscoreSeparatedRearrangeInstructionData] =
+    rawFileData.toString().split('\n').join('_').split('__');
+  const rearrangeInstructions = underscoreSeparatedRearrangeInstructionData
+    .split('_')
+    .filter(x => !!x);
   // we've looked at the file, so its kinda like cheating.. but whatever
   const containerMap = initializeContainerMap(containerData.split('_'));
-  
+  rearrangeStacks(rearrangeInstructions, containerMap);
   // After the rearrangement procedure completes, what crate ends up on top of each stack?
   const topOfStack: string[] = [];
-  console.log(topOfStack);
+  for (let i = 1; i <= containerMap.size; i++) {
+    const stack = containerMap.get(i) as string[];
+    const lastThing = stack[stack.length - 1];
+    topOfStack.push(lastThing);
+  }
+  console.log(topOfStack.join(''));
   return topOfStack.join('');
-}
-
+};
 
 // for node runs
 main();
--- 05/src/tests/index.spec.ts
+++ 05/src/tests/index.spec.ts
@@ -1,5 +1,9 @@
-import { expect } from 'chai';
-import { getContainerAtPosition, initializeContainerMap, main } from '../index';
+import {expect} from 'chai';
+import {
+  getContainerAtPosition,
+  initializeContainerMap,
+  moveStacks,
+} from '../index';
 
 describe(`index`, () => {
   describe(`getContainerAtPosition`, () => {
@@ -17,7 +21,9 @@ describe(`index`, () => {
         [9, 'D'],
       ];
       expectedResults.forEach(([position, expectedResult]) => {
-        expect(getContainerAtPosition(input, +position)).to.equal(expectedResult);
+        expect(getContainerAtPosition(input, +position)).to.equal(
+          expectedResult
+        );
       });
     });
   });
@@ -32,7 +38,7 @@ describe(`index`, () => {
         '[J] [V] [T] [N] [F] [G] [Z] [S] [S]',
         '[C] [R] [P] [S] [V] [M] [V] [D] [Z]',
         '[F] [G] [H] [Z] [N] [P] [M] [N] [D]',
-        '1   2   3   4   5   6   7   8   9 '
+        '1   2   3   4   5   6   7   8   9 ',
       ];
       const expectedResults = JSON.stringify([
         ['F', 'C', 'J', 'P', 'H', 'T', 'W'],
@@ -43,19 +49,28 @@ describe(`index`, () => {
         ['P', 'M', 'G', 'F', 'W', 'D', 'Z'],
         ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'],
         ['N', 'D', 'S'],
-        ['D', 'Z', 'S', 'F', 'M']
+        ['D', 'Z', 'S', 'F', 'M'],
       ]);
-      const containerData = JSON.stringify(initializeContainerMap(input));
+      const containerData = JSON.stringify([
+        ...initializeContainerMap(input).values(),
+      ]);
       console.log({expectedResults, containerData});
       expect(containerData).to.equal(expectedResults);
-      // expectedResults.forEach((contents, i) => {
-      //   for (let col = 0; col < 9; col++) {
-      //     const expected = containerData[i][col];
-      //     const actual = contents[i][col];
-      //     expect(actual).to.equal(expected);
-      //     console.log({expected, actual });
-      //   }
-      // });
+    });
+  });
+  describe(`moveStacks`, () => {
+    it(`should move stack and retain original order`, () => {
+      const map = new Map<number, string[]>([
+        [1, ['A', 'B', 'C']],
+        [2, ['D', 'E', 'F']],
+      ]);
+      moveStacks(2, 1, 2, map);
+      const expectedStackOne = 'A';
+      const expectedStackTwo = 'DEFBC';
+      const actualStackOne = map.get(1)?.join('');
+      const actualStackTwo = map.get(2)?.join('');
+      expect(expectedStackOne).to.equal(actualStackOne);
+      expect(expectedStackTwo).to.equal(actualStackTwo);
     });
   });
 });
